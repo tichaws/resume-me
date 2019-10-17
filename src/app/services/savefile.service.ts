f4c8ebb07888912a455aa0eb971a3ff251138274
Laphatrada Wisadsri <zlaphatrada@tisco.co.th>
2019-10-17 14:21:36 +0700

    Add new version resume

--- src/app/services/savefile.service.ts
+++ src/app/services/savefile.service.ts
@@ -16,7 +16,7 @@ export class SavefileService {
 
   dowloadFile() {
     this.http
-        .get<any>("/assets/pdf/resume-laphatrada.pdf", { responseType: 'blob' as 'json' })
+        .get<any>("/assets/pdf/LAPHAT-RADA's-Resume.pdf", { responseType: 'blob' as 'json' })
         .subscribe(data => {
           FileSaver.saveAs(data,'laphatrada-resume');
         });

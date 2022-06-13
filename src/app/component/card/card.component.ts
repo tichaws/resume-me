7b5af5e224dbae0303e77eea70772cc5416e89bb
Laphatrada W <tichaws@gmail.com>
2022-06-14 01:21:00 +1000

    add shuffle function

--- src/app/component/card/card.component.ts
+++ src/app/component/card/card.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Model } from 'src/app/model/model';
+import { Project } from '../detail/detail';
 
 @Component({
   selector: 'app-card',
@@ -9,9 +10,16 @@ import { Model } from 'src/app/model/model';
 export class CardComponent implements OnInit {
 
   constructor(public models:Model) { }
-
+  public projects:Array<Project> = this.models.project
   ngOnInit() {
 
   }
 
+  btnShuffle(){
+    /* use Math.random() to get a number between 0 and 1 then reduce .5 
+    to set the lower and upper bounds */
+    this.projects.sort(()=> Math.random() - 0.5)
+    /*  If the result of this operation is < 0, the element a is put to an index lower than b,
+    and the opposite if the result is > 0.  */
+  }
 }
